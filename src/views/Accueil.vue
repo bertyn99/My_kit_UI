dfe55f474ab517d0ece2305969fba0e2d7af5c2f
bertyn <bertynboulikou@gmail.com>
2021-01-06 20:11:37 +0100

    creaction des view

---
+++ src/views/Accueil.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button></Button>
+  <Navbar></Navbar>
+</template>
+
+<script>
+import Button from "./Button";
+import Navbar from "./Navbar";
+export default {
+  name: "Accueil",
+  components: { Button, Navbar },
+};
+</script>
+
+<style>
+</style>
